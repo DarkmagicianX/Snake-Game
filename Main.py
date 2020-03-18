186a80a9a4fac37a8df95ce038032e06df943283
DarkmagicianX <50853069+DarkmagicianX@users.noreply.github.com>
2020-03-18 00:16:47 -0700

    SnakeGame.py
    Main

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+
+import os 
+import turtle
+import time
+import random 
+
+#set up the screen 
+wn = turtle.Screen()
+wn.title("Snake Game by Arvin Setareh")
+wn.bgcolor ("blue")
+wn.setup (width=600, height = 600) 
+wn.tracer (0)
+
+wn.mainloop()
+
